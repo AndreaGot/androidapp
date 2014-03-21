7535ee88d43abf6b21856789c1c32f45d4705569
Andrea Gottardi <andreagot16@gmail.com>
2014-03-21 12:45:09 +0100

    Added Intent Test

---
+++ AndroidIntentTest/app/src/main/java/com/scigot/intents/SecondActivity.java
@@ -0,0 +1,60 @@
+package com.scigot.intents;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        final TextView txtParam = (TextView) findViewById(R.id.txtShowParameter);
+        Intent data = getIntent();
+        Bundle extras = getIntent().getExtras();
+        if (extras != null)
+        {
+            String param = extras.getString("intentParameter");
+            Toast.makeText(SecondActivity.this, param, Toast.LENGTH_LONG).show();
+            txtParam.setText(param);
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
